b8c57c2afa3138611bf4a41597344a790389b60f
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-05-27 00:13:54 -0700

    Add example ES6 Class

---
+++ js/controller.js
@@ -0,0 +1,15 @@
+export default class Controller {
+
+	constructor() {
+
+	}
+
+	update() {
+		// TODO: Some updating logic
+	}
+
+	render(context, scale, xOffset, yOffset) {
+		// TODO: Some rendering logic
+	}
+
+}
