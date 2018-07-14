aad89fe8441bfd2fe1db09b6bc632d703de14435
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-07-14 09:23:38 +0800

    Add the easing functions we know and love

---
+++ js/easing.js
@@ -0,0 +1,32 @@
+export function easeInOut(t, amt=2) {
+    let tPow = Math.pow(t, amt);
+    return tPow / (tPow + Math.pow(1 - t, amt));
+}
+
+export function sinEaseInOut(t) {
+	return 0.5 - 0.5 * Math.cos(Math.PI * t);
+}
+
+export function slurp(val1, val2, amt) {
+    return (val2 - val1) * amt + val1;
+}
+
+export function clampedSlurp(val1, val2, amt) {
+    if (amt < 0) {
+        return val1;
+    }
+    if (amt > 1) {
+        return val2;
+    }
+    return slurp(val1, val2, amt);
+}
+
+export function clamp(amt, val1, val2) {
+    if (amt < 0) {
+        return val1;
+    }
+    if (amt > 1) {
+        return val2;
+    }
+    return amt;
+}
