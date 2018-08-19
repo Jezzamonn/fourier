e1f69a4813e7ab3285c8d33af62cca2a35b24060
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-08-19 01:06:25 -0700

    Little script to generate points from a SVG path

---
+++ script/get-points.js
@@ -0,0 +1,13 @@
+let path = document.getElementsByTagName('path')[0];
+let length = path.getTotalLength()
+let numPoints = 1024;
+let points = [];
+for (let i = 0; i < numPoints; i ++) {
+    point = path.getPointAtLength(length * i / numPoints);
+    points.push(point);
+    // s += '' + point.x + ',' + point.y + '\n'
+}
+
+// Turn into a string of things
+let s = points.reduce((s, point) => s + point.x + ',' + point.y + '\n');
+console.log(s);
