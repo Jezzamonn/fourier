97e0ef29afb75aee0bc2ed78ff2a57640b020b1b
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-08-19 18:47:09 -0700

    Separate configuring controllers from animation/input
    Create a class called conductor that has a list of controllers to update. Then main.js just inits the controllers and passes them in :)

---
+++ js/conductor.js
@@ -0,0 +1,55 @@
+export default class Conductor {
+
+	constructor(controllers) {
+		this.lastTime = Date.now();
+		this.mousePosition = null;
+		this.controllers = controllers.slice();
+
+		// We can handle these all the same really.
+		document.addEventListener('mousemove', (evt) => this.updateMousePosition(evt));
+		document.addEventListener('mousedown', (evt) => this.updateMousePosition(evt));
+		document.addEventListener('mouseup',   (evt) => this.updateMousePosition(evt));
+
+		document.addEventListener('touchmove',  (evt) => this.updateTouchPosition(evt));
+		document.addEventListener('touchstart', (evt) => this.updateTouchPosition(evt));
+		document.addEventListener('touchend',   (evt) => this.updateTouchPosition(evt));
+	}
+
+	start() {
+		// Kick off the update loop
+		window.requestAnimationFrame(() => this.everyFrame());
+	}
+
+	everyFrame() {
+		this.update();
+		this.render();
+		requestAnimationFrame(() => this.everyFrame());
+	}
+
+	update() {
+		let curTime = Date.now();
+		let dt = (curTime - this.lastTime) / 1000;
+
+		this.controllers.forEach(controller => {
+			controller.update(dt, this.mousePosition);
+		});
+
+		this.lastTime = curTime;
+	}
+
+	render() {
+		this.controllers.forEach(controller => {
+			controller.render();
+		});
+	}
+
+	updateMousePosition(evt) {
+		this.mousePosition = { x: evt.clientX, y: evt.clientY };
+	}
+
+	updateTouchPosition(evt) {
+		this.mousePosition = { x: evt.touches[0].clientX, y: evt.touches[0].clientY };
+		evt.preventDefault();
+	}
+
+}
--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 import DrawController from './draw-controller.js';
 import EpicyclesController from './epicycles-controller.js';
 import WaveController from './wave-controller.js';
+import Conductor from './conductor.js';
 
-let lastTime;
-let controllers;
-let mousePosition;
+let conductor = null;
 
 function init() {
-	lastTime = Date.now();
-	controllers = [];
-
 	let drawZone, circles, epicycles, waves;
+	let controllers = [];
 	if (hasElement('drawzone')) {
 		drawZone = new DrawController('drawzone', 500, 500);
 		window.drawZone = drawZone;
@@ -38,54 +35,12 @@ function init() {
 		controllers.push(waves);
 	}
 
-	// We can handle these all the same really.
-	document.addEventListener('mousemove', updateMousePosition);
-	document.addEventListener('mousedown', updateMousePosition);
-	document.addEventListener('mouseup', updateMousePosition);
-
-	document.addEventListener('touchmove', updateTouchPosition);
-	document.addEventListener('touchstart', updateTouchPosition);
-	document.addEventListener('touchend', updateTouchPosition);
-
-	// Kick off the update loop
-	window.requestAnimationFrame(everyFrame);
+	conductor = new Conductor(controllers);
+	conductor.start();
 }
 
 function hasElement(id) {
 	return document.getElementById(id) != null;
-}
-
-// TODO: Make tweak this to allow frame skipping for slow computers. Maybe.
-function everyFrame() {
-	update();
-	render();
-	requestAnimationFrame(everyFrame);
-}
-
-function update() {
-	let curTime = Date.now();
-	let dt = (curTime - lastTime) / 1000;
-
-	controllers.forEach(controller => {
-		controller.update(dt, mousePosition);
-	});
-	
-	lastTime = curTime;
-}
-
-function render() {
-	controllers.forEach(controller => {
-		controller.render();
-	});
-}
-
-function updateMousePosition(evt) {
-	mousePosition = {x: evt.clientX, y: evt.clientY};
-}
-
-function updateTouchPosition(evt) {
-	mousePosition = {x: evt.touches[0].clientX, y: evt.touches[0].clientY};
-	evt.preventDefault();
 }
 
 init();
