9e6c02f5a3fbaabb7de17ed25b2d6a06f1e54a26
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-08-31 19:08:46 -0700

    Got a fun to play with drawing thing working :)

--- js/controller/wave-draw-controller.js
+++ js/controller/wave-draw-controller.js
@@ -1,17 +1,17 @@
 import Controller from "../controller";
+import { slurp } from "../util";
  
-const maxDrawDist = 3;
-
 export default class WaveDrawController extends Controller {
 
 	constructor(id, width, height) {
         super(id, width, height);
         // just a list
-        this.wavePoints = [0, 10, 500, 490];
+        this.wavePoints = new Array(128).fill(this.height / 2);
 
         this.drawing = false;
         this.onDrawingStart = [];
         this.onDrawingEnd = [];
+        this.lastMousePoint = null;
 
         this.canvas.addEventListener('mousedown', () => this.startDrawing());
         this.canvas.addEventListener('touchstart', () => this.stopDrawing());
@@ -22,12 +22,14 @@ export default class WaveDrawController extends Controller {
 
    startDrawing() {
         this.drawing = true;
+        this.lastMousePoint = null;
 
         this.onDrawingStart.forEach(fn => fn());
     }
 
     stopDrawing() {
         this.drawing = false;
+        this.lastMousePoint = null;
 
         this.onDrawingEnd.forEach(fn => fn());
     }
@@ -39,12 +41,38 @@ export default class WaveDrawController extends Controller {
 
         // TODO: Some minimum point length
         const canvasPosition = this.canvas.getBoundingClientRect();
-        const point = {
+        const mousePoint = {
             x: mousePosition.x - canvasPosition.x,
             y: mousePosition.y - canvasPosition.y,
         }
+        if (this.lastMousePoint == null) {
+            this.lastMousePoint = mousePoint;
+        }
 
+        const xDiff = Math.abs(mousePoint.x - this.lastMousePoint.x);
+        const pointsGap = this.width / this.wavePoints.length;
+        const lerpPoints = 2 * Math.ceil(xDiff / pointsGap);
+        for (let i = 0; i < lerpPoints; i ++) {
+            const amt = (i - 1) / lerpPoints;
 
+            const index = this.getNearestIndex(slurp(this.lastMousePoint.x, mousePoint.x, amt));
+            this.wavePoints[index] = slurp(this.lastMousePoint.y, mousePoint.y, amt);
+        }
+
+        this.lastMousePoint = mousePoint;
+    }
+    
+    /**
+     * Gets the nearest index in the wave array to the x coord on the screen
+     * @param {Number} x 
+     */
+    getNearestIndex(x) {
+        const xAmt = (x / this.width)
+        let pos = Math.round(this.wavePoints.length * xAmt) % this.wavePoints.length;
+        if (pos < 0) {
+            pos += this.wavePoints.length;
+        }
+        return pos;
     }
     
 	render() {
