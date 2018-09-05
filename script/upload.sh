a180019d38e5b9028132a2ac334e78d43fd7cf44
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-09-04 19:07:34 -0700

    Update upload script

--- script/upload.sh
+++ script/upload.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-gsutil -m cp *.html gs://www.jezzamon.com/{{PATH}}/
-gsutil -m cp -r build/ gs://www.jezzamon.com/{{PATH}}/
-gsutil -m cp -r css/ gs://www.jezzamon.com/{{PATH}}/
-gsutil -m acl ch -r -u AllUsers:R gs://www.jezzamon.com/{{PATH}}
+usage="Usage: $(basename "$0") path -- Uploads to the www.jezzamon.com/somepath"
+
+if [ "$#" -ne 1 ]; then
+    echo "$usage"
+    exit 1
+fi
+
+path=$1
+
+# TODO: Update HTML, etc so the path gets updated
+
+gsutil -m cp -r build/* gs://www.jezzamon.com/$path/
+gsutil -m acl ch -r -u AllUsers:R gs://www.jezzamon.com/$path
