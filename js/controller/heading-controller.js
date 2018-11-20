fa3f993698416e639f785adf98bc633f68ac5b0d
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-11-19 23:16:17 -0800

    Make the heading mildly interactive

--- js/controller/heading-controller.js
+++ js/controller/heading-controller.js
@@ -9,6 +9,7 @@ export default class HeadingController extends CanvasController {
         super(id, width, height);
 
         this.animAmt = 0;
+        this.mousePos = null;
 
         this.onResize();
     }
@@ -24,6 +25,9 @@ export default class HeadingController extends CanvasController {
 
 	update(dt, mousePosition) {
         this.animAmt += dt;
+        if (mousePosition) {
+            this.mousePos = Object.assign({}, mousePosition);
+        }
     }
 
 	render() {
@@ -35,18 +39,27 @@ export default class HeadingController extends CanvasController {
 
         const numLines = 4;
         for (let j = 0; j < numLines; j ++) {
-            const waveTop = this.height * j / numLines;
-            const waveBottom = this.height * (j + 1) / numLines;
+            const waveHeight = this.height / numLines;
+            const waveTop = (j + 0) * waveHeight;
+            const waveMiddle = (j + 0.5) * waveHeight;
+            const waveBottom = (j + 1) * waveHeight;
 
             const freq = 2 * Math.pow(2, (numLines - 1) - j);
             const speed = 0.05 + 0.02 * j;
 
             for (let i = 0; i <= this.width; i += 3) {
                 // some some arbitray mapping to real world things
-                const t = i / 500 + speed * this.animAmt;
+                let t = i / 500;
+                const x = i;
+                if (this.mousePos) {
+                    const xDiff = (this.mousePos.x - x);
+                    const yDiff = (this.mousePos.y - waveMiddle);
+                    const stretchAmt = 0.5 * gaussianLike(yDiff, waveHeight);
+                    t = stretch(t, xDiff / 500, 0.2, stretchAmt);
+                }
+                t += speed * this.animAmt;
                 const waveAmt = 0.5 + 0.5 * Math.sin(2 * Math.PI * freq * t);
     
-                const x = i;
                 const y = slurp(waveTop, waveBottom, slurp(0.1, 0.9, waveAmt));
     
                 if (i == 0) {
@@ -60,5 +73,14 @@ export default class HeadingController extends CanvasController {
         this.context.stroke();
         this.context.globalAlpha = 1;
     }
+}
 
+function gaussianLike(x, width) {
+    // A gaussian type thing. It equals 1 at 0 (instead of having a total area of 1)
+    return Math.exp(-0.5 * (x * x) / (width * width))
+}
+
+function stretch(t, diff, width, strength) {
+    const stretchAmt = strength * diff * gaussianLike(diff, width);
+    return t + stretchAmt;
 }
