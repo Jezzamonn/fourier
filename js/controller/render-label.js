5fcfadfe70d68809a4058789a9ee7de377bc5994
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-11-21 09:41:21 -0800

    Render multiline labels properly

--- js/controller/render-label.js
+++ js/controller/render-label.js
@@ -13,23 +13,43 @@ export function renderLabel(context, text, x, y, labelDist, color, minX, maxX, t
     context.strokeStyle = color;
     context.fillStyle = color;
 
+    const lines = text.split('\n');
+
+    let textWidth = 0;
     context.font = '16px "Open Sans", sans-serif';
-    const textMetrics = context.measureText(text);
-    const textWidth = textMetrics.width;
+    for (const line of lines) {
+        const textMetrics = context.measureText(line);
+        if (textMetrics.width > textWidth) {
+            textWidth = textMetrics.width;
+        }
+    }
+    // Just hard code this because measuring it is more complicated
+    let textHeight = 20;
 
     // For the moment, set it to what it would be if it was facing the right
-    let labelX = x + labelDist;
-    const labelY = y - labelDist;
+    let labelX = 0;
+    let labelY = 0;
+    let textX = 0;
+    let textY = 0;
 
     if (labelX + textWidth + textPadding < maxX) {
         // slanting to the right
         labelX = x + labelDist;
-        context.fillText(text, labelX + textPadding, labelY - textPadding);
+        labelY = y - labelDist;
+        textX = labelX + textPadding
+        textY = labelY - textPadding;
     }
     else {
         // slanting to the left
-        labelX = x - labelDist;
-        context.fillText(text, labelX - textWidth - textPadding, labelY - textPadding)
+        labelX = x + labelDist;
+        labelY = y - labelDist;
+        textX = labelX - textWidth - textPadding;
+        textY = labelY - textPadding;
+    }
+    for (let i = 0; i < lines.length; i ++) {
+        const line = lines[i];
+        const lineOffset = textHeight * ((lines.length - 1) - i)
+        context.fillText(line, textX, textY - lineOffset);
     }
 
     context.beginPath();
