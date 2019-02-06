5e9cf697e5c50e58e36b4e3eb68d5f0b536f1f28
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-02-06 09:35:54 -0800

    Update build scripts to deal with new content directory

--- script/make-html.js
+++ script/make-html.js
@@ -4,36 +4,41 @@ const fs = require('fs');
 
 const pageData = [
     { // English
-        markdown: 'content.md',
+        languageName: 'English',
+        markdownFileName: 'content.md',
         title: 'An Interactive Introduction to Fourier Transforms',
         description: 'Fourier transforms are a tool used in a whole bunch of different things. This is a explanation of what a Fourier transform does, and some different ways it can useful. And how you can make pretty things with it, like this thing:',
-        filename: 'index.html',
+        outFileName: 'index.html',
         url: '',
     },
-    { // Spanish!??! 😲
-        markdown: 'content-es.md',
+    { // Spanish
+        languageName: 'Español',
+        markdownFileName: 'content-es.md',
         title: 'Una introducción interactiva a las transformadas de Fourier',
         description: 'Las transformadas de Fourier son una herramienta utilizada en un montón de cosas diferentes. Esta es una explicación de lo que hace una transformada de Fourier, algunas formas diferentes en que puede ser útil y cómo puedes hacer cosas bonitas con ella, como esta cosa:',
-        filename: 'es.html',
+        outFileName: 'es.html',
         url: '/es.html',
     },
     { // Debug
-        markdown: 'debug.md',
+        markdownFileName: 'debug.md',
         title: 'debug debug debug',
         description: 'debugbugbugbugbugbugbugbug',
-        filename: 'debug.html',
+        outFileName: 'debug.html',
         url: '/debug.html',
     },
 ]
+
+const contentDir = 'content/'
+const buildDir = 'build/';
 
 const markdownControler = new showdown.Converter();
 
 const template = fs.readFileSync('template.html').toString();
 
 for (pageDatum of pageData) {
-    console.log(`generating ${pageDatum.filename}`)
+    console.log(`generating ${pageDatum.markdownFileName}`)
     // Read in content
-    markdown = fs.readFileSync(pageDatum.markdown).toString();
+    markdown = fs.readFileSync(contentDir + pageDatum.markdownFileName).toString();
 
     // Convert to html
     htmlContent = markdownControler.makeHtml(markdown);
@@ -44,5 +49,5 @@ for (pageDatum of pageData) {
 
     html = mustache.render(template, view)
     // Output to build directory;
-    fs.writeFileSync('build/' + pageDatum.filename, html)
+    fs.writeFileSync(buildDir + pageDatum.outFileName, html)
 }
