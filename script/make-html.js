12684e69dd52d18c7459a69ad19722fb277546a2
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-02-06 18:31:51 -0800

    Add credit for the wonderful translator(s)!

--- script/make-html.js
+++ script/make-html.js
@@ -10,6 +10,7 @@ const pageData = [
         description: 'Fourier transforms are a tool used in a whole bunch of different things. This is a explanation of what a Fourier transform does, and some different ways it can useful. And how you can make pretty things with it, like this thing:',
         outFileName: 'index.html',
         url: '',
+        translatorMarkdown: '',
     },
     { // Spanish
         languageName: 'Español',
@@ -18,6 +19,7 @@ const pageData = [
         description: 'Las transformadas de Fourier son una herramienta utilizada en un montón de cosas diferentes. Esta es una explicación de lo que hace una transformada de Fourier, algunas formas diferentes en que puede ser útil y cómo puedes hacer cosas bonitas con ella, como esta cosa:',
         outFileName: 'es.html',
         url: '/es.html',
+        translatorMarkdown: 'Traducido por [Juan Carlos Ponce Campuzano](https://www.jcponce.com)',
     },
     { // Debug
         markdownFileName: 'debug.md',
@@ -25,29 +27,32 @@ const pageData = [
         description: 'debugbugbugbugbugbugbugbug',
         outFileName: 'debug.html',
         url: '/debug.html',
+        translatorMarkdown: '',
     },
 ]
 
 const contentDir = 'content/'
 const buildDir = 'build/';
 
-const markdownControler = new showdown.Converter();
+const markdownConverter = new showdown.Converter();
 
 const template = fs.readFileSync('template.html').toString();
 
 for (pageDatum of pageData) {
     console.log(`generating ${pageDatum.markdownFileName}`)
     // Read in content
-    markdown = fs.readFileSync(contentDir + pageDatum.markdownFileName).toString();
+    const markdown = fs.readFileSync(contentDir + pageDatum.markdownFileName).toString();
 
     // Convert to html
-    htmlContent = markdownControler.makeHtml(markdown);
+    const htmlContent = markdownConverter.makeHtml(markdown);
+    const translator = markdownConverter.makeHtml(pageDatum.translatorMarkdown);
 
     // Fill into template
-    view = Object.assign({}, pageDatum);
+    const view = Object.assign({}, pageDatum);
     view.content = htmlContent;
+    view.translator = translator;
 
-    html = mustache.render(template, view)
+    const html = mustache.render(template, view)
     // Output to build directory;
     fs.writeFileSync(buildDir + pageDatum.outFileName, html)
 }
