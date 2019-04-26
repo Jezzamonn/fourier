4f6540bf8af15c97f553e0fceff50d39df1c7ffd
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-04-26 09:37:59 -0700

    Add data for the chinese translation

--- script/make-html.js
+++ script/make-html.js
@@ -39,6 +39,15 @@ const pageData = [
         url: '/pt_br.html',
         translatorMarkdown: 'Traduzido por [Jean Oliveira Rodrigues de Araujo](https://twitter.com/_jaraujo_)',
     },
+	{ // Chinese (simplified)
+        languageName: '简体中文',
+        markdownFileName: 'content-zh-cn.md',
+        title: '傅里叶变换交互式入门',
+        description: '傅里叶变换是一种在各个领域都经常使用的数学工具。这个网站将为你介绍傅里叶变换能干什么，为什么傅里叶变换非常有用，以及你如何利用傅里叶变换干漂亮的事。',
+        outFileName: 'zh-cn.html',
+        url: '/zh-cn.html',
+        translatorMarkdown: '',
+    },
     { // Debug
         markdownFileName: 'debug.md',
         title: 'debug debug debug',
