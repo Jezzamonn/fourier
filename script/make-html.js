30c965679d54c8f9f8a56f14569678b5b1652f47
Straaths <straaths@gmail.com>
2019-08-20 15:49:49 +0200

    Small content fix in English language.
    Missing be word.

--- script/make-html.js
+++ script/make-html.js
@@ -7,7 +7,7 @@ const pageData = [
         languageName: 'English',
         markdownFileName: 'content.md',
         title: 'An Interactive Introduction to Fourier Transforms',
-        description: 'Fourier transforms are a tool used in a whole bunch of different things. This is a explanation of what a Fourier transform does, and some different ways it can useful.',
+        description: 'Fourier transforms are a tool used in a whole bunch of different things. This is a explanation of what a Fourier transform does, and some different ways it can be useful.',
         outFileName: 'index.html',
         url: '',
         translatorMarkdown: '',
