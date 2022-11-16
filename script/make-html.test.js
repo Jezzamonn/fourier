394efc3d661892ce802c6b5e81e492d3480439fa
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2022-11-16 14:23:43 -0800

    Tweak how tests are structured a little

--- script/make-html.test.js
+++ script/make-html.test.js
@@ -12,44 +12,44 @@ async function tmpDir() {
 // Just test everything!
 describe('exportAllLanguages', () => {
 
+    const expectedFiles = [
+        "de.html",
+        "debug.html",
+        "es.html",
+        "fr.html",
+        "he.html",
+        "index.html",
+        "it.html",
+        "ko.html",
+        "pl.html",
+        "pt-br.html",
+        "pt_br.html",
+        "sk.html",
+        "tr.html",
+        "zh-cn.html",
+        "zh-tw.html",
+    ];
+
     it('has the correct output files', async () => {
         const dir = await tmpDir();
 
         exportAllLanguages({outputDir: dir});
         const files = await fs.readdir(dir);
         files.sort();
-        // Lazy :)
-        expect(files).toMatchInlineSnapshot(`
-[
-  "de.html",
-  "debug.html",
-  "es.html",
-  "fr.html",
-  "he.html",
-  "index.html",
-  "it.html",
-  "ko.html",
-  "pl.html",
-  "pt-br.html",
-  "pt_br.html",
-  "sk.html",
-  "tr.html",
-  "zh-cn.html",
-  "zh-tw.html",
-]
-`);
+
+        expect(files).toEqual(expectedFiles);
     });
 
-    it('outputs the correct HTML for each language', async () => {
-        const dir = await tmpDir();
-        exportAllLanguages({outputDir: dir});
+    // Generate tests for each file.
+    for (const file of expectedFiles) {
+        it(`has the correct contents for ${file}`, async () => {
+            const dir = await tmpDir();
+            exportAllLanguages({outputDir: dir});
 
-        // Compare each file with a snapshot
-        const files = await fs.readdir(dir);
-        for (const file of files) {
+            // Compare file with snapshot.
             const filePath = path.join(dir, file);
-            const html = await fs.readFile(filePath, 'utf-8');
-            expect(html).toMatchSnapshot(file);
-        }
-    });
+            const contents = await fs.readFile(filePath, 'utf-8');
+            expect(contents).toMatchSnapshot(file);
+        });
+    }
 });
